930374a6c54f2a31313d7a77f0cfe075e54e9faa
PelicanAppa <145051119+PelicanAppa@users.noreply.github.com>
2024-01-11 18:43:35 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hi, I am console screen.");
+var first_name="Vrushank";
+console.log(first_name);
+var last_name="Padaki";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var squareroot=Math.sqrt(2);
+console.log(squareroot);
+var maximumnumber=Math.max(4, 8, 12, 16, 20);
+console.log(maximumnumber);
+var minimumnumber=Math.min(4, 16, 32, 64);
+console.log(minimumnumber);
+var power=Math.pow(4, 3)
+console.log(power);
